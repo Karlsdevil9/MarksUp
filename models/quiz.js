f92d13d78ca07e9600c26bec99233fd8d730fa8b
@rytest32 <rytest32@gmail.com>
2021-11-30 22:38:41 +0530

    commit

--- models/quiz.js
+++ models/quiz.js
@@ -19,6 +19,12 @@ const quizSchema = new mongoose.Schema({
         required: true,
         maxlength: 60
     },
+    examDate: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 100
+    },
     endTime: {
         type: String,
         trim: true,
